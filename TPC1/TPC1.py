5c876c48a178a7fe15cebb0d08acc68f2b286109
Alpha241 <oliveirapedro.alexandres@gmail.com>
2023-02-16 19:45:12 +0000

    Estruturação e validação de dados completa

--- TPC1/TPC1.py
+++ TPC1/TPC1.py
@@ -1,5 +1,4 @@
 
-
 def main():
     readfile()
 
@@ -12,9 +11,43 @@ def readfile():
     colestrol = dict()
     batimento = dict()
     temDoenca = dict()
+    
+    #define a chave do dicionário
+    key = 0
 
     with open('myheart.csv', 'r') as file:
         lines = file.readlines()[1:]
         for line in lines:
+            paciente = line.strip().split(",")
+            if validaDados(paciente) == True:
+                idade[key] = paciente[0]
+                sexo[key] = paciente[1]
+                tensao[key] = paciente[2]
+                colestrol[key] = paciente[3]
+                batimento[key] = paciente[4]
+                temDoenca[key] = paciente[5]
+                key += 1
+        
+        
+            
+
             
     
+def validaDados(lista):
+    if int(lista[0]) < 0:
+        return False
+    elif lista[1] != 'M' and lista[1] != 'F':
+        return False
+    elif int(lista[2]) <= 0:
+        return False
+    elif int(lista[3]) <= 0:
+        return False
+    elif int(lista[4]) <= 0:
+        return False
+    elif int(lista[5]) != 0 and int(lista[5]) != 1:
+        return False
+    else:
+        return True
+    
+
+main()
