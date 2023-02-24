e1be1da4c04e76b3ae79fbcf7af99943205c494f
pedro.0liveira <a98712@alunos.uminho.pt>
2023-02-24 17:08:57 +0000

    Update

--- TPC2/TPC2.py
+++ TPC2/TPC2.py
@@ -7,13 +7,25 @@ def main():
     while True:
         res = 0
         txt = input("Introduza o texto (fim para terminar): ")
+        status = False
         if 'fim' == txt:
             break
         
+        txt = txt.upper().strip().replace(' ','')
         for caractere in txt:
-            if caractere.isdigit():
-                res += int(caractere) 
+            if caractere.isdigit() and status is False:
+                res += int(caractere)
+            if caractere == '=' and status is False:
+                print(res)
+            #if txt[caractere:caractere+2] == "ON":
+                #status = False
+            #if txt[caractere:caractere+3] == "OFF":
+                #caractere = caractere + 3
+        
         print(res)
+            
+        
+
         
     
 
